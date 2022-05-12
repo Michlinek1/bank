9d37e449ccdee1235b175ac6c0796d5d408f3f99
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-05-12 20:31:00 +0200

    Update logowanie.php

--- logowanie.php
+++ logowanie.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+error_reporting(E_ALL & ~E_NOTICE);
 
 ?>
     <!DOCTYPE html>
@@ -9,15 +10,18 @@ session_start();
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel = "stylesheet " href = "style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script  src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.js"></script>
         <script defer src = "scriptlogowanie.js"></script>
+    
         <title>Zaloguj</title>  
     </head>
     <body>
         
         <form class = "login-form"  method = "post">
         <div class = "container">
-            
+            <!-- bank logo -->
             <div class = "login_logo">
             <i class = "fa fa-bank"=></i>
             </div>
@@ -31,12 +35,17 @@ session_start();
         <br>
         <input type ="checkbox" id =  "pokazhaslo" onclick = "pokaz_haslo()">Pokaż hasło
         <br> <br>
-        <button class = "login_przycisk"type = "submit" name = "zaloguj" value = "Zaloguj">Zaloguj</button>
+        <button class = "login_przycisk"type = "submit" name = "zaloguj" value = "Zaloguj" >Zaloguj</button>
         <br> <br>
         <div class = "login-form_link">
         <a href = "register.php" class = "login_link">Zarejestruj</a>
         <?php
         if(isset($_POST['zaloguj'])){
+            if($_POST['mail'] == "" || $_POST['password'] == ""){
+                echo '<script>'
+                .'swal("Błąd", "Wprowadź poprawne dane", "error")'
+                .'</script>';
+            }else{
             $pol = new mysqli('localhost', 'root', '', 'bank');
             $sql = $pol->query("SELECT * FROM uzytkownik WHERE email = '$_POST[mail]'");
             $row = $sql->fetch_assoc();
@@ -46,11 +55,15 @@ session_start();
                 $_SESSION['login'] = $row['login'];
                 header("Location: index.php?logged=true");
             }else{
-                echo "<script>document.getElementById('bledy').innerHTML = 'Błędne hasło lub email!';</script>";
-                echo "<script>document.getElementsByClassName('login_logowanie')[0].style.borderColor = 'red';</script>";
-                echo "<script>document.getElementsByClassName('login_logowanie')[1].style.borderColor = 'red';</script>";
+                echo '<script>'
+                .'swal("Błąd", "Wprowadz poprawny email oraz hasło", "error")'
+                .'</script>';
             }
         }
+        }
+    
+    
+        
         
 
           
